a9e99ab0ea65a86c72b694c403c28eec10c111df
kuhaku001 <jorge.fj.carcamo@gmail.com>
2023-12-24 15:13:26 -0300

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom";
 export function Header() {
     return(
         <div>
-            <Link to="/" >Incio</Link>
-            <Link to="/perfil" >Perfil</Link>
+            <ol>
+                <li>
+                    <Link to="/" >Incio</Link>
+                </li>
+                <li>
+                    <Link to="/perfil" >Perfil</Link>
+                </li>
+            </ol>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
-import { App } from './App.jsx'
-import { Header } from './components/Header.jsx'
+import App from './App.jsx'
 
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <>
-      <Header />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
