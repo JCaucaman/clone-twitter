09d89df2edfa460a7aac9c373757b236b6b70b70
kuhaku001 <jorge.fj.carcamo@gmail.com>
2023-12-25 22:30:26 -0300

    add new styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      maxHeight: {
+        128: "32rem",
+      },
+      maxWidth: {
+        128: "32rem",
+      },
+      height: {
+        41: "41.5rem",
+      },
+      width: {
+        37: "37.4rem",
+      },
+    },
   },
   plugins: [],
 };
